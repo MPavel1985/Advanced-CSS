d07fb89ec0863ba22b3f2fe63f07c892f8088901
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2023-05-10 18:32:58 +0300

    created folders, gulpfile and tasks

--- HW-2/gulpfile.js
+++ HW-2/gulpfile.js
@@ -6,66 +6,49 @@ const uglify = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 const clean = require("gulp-clean");
-// * !not for all browsers!
 const avif = require("gulp-avif");
-// * I can't inctall it + gulp-cwebp
-// const webp = require("gulp-webp");
-// * gulp-imagemin works with PNG, JPEG, GIF and SVG
-// const imagemin = require("gulp-imagemin");
 // * for not using pictures with which we have worked earlier - plug for cache
 const newer = require("gulp-newer");
 const fonter = require("gulp-fonter");
 const ttf2woff2 = require("gulp-ttf2woff2");
-const include = require("gulp-include");
-
-function makePages() {
-  return src("app/pages/*.html")
-    .pipe(
-      include({
-        includePaths: "app/components",
-      })
-    )
-    .pipe(dest("app"))
-    .pipe(browserSync.stream());
-}
 
 function fonts() {
-  return src("app/fonts/*.*")
+  return src("src/fonts/*.*")
     .pipe(
       fonter({
         formats: ["woff", "ttf"],
       })
     )
-    .pipe(src("app/fonts/*.ttf"))
+    .pipe(src("src/fonts/*.ttf"))
     .pipe(ttf2woff2())
-    .pipe(dest("app/fonts"));
+    .pipe(dest("src/fonts"));
 }
 
 function images() {
   return (
-    src("app/images-app/**/*.*")
-      // if we need to use one more plug for another type of images, have to use before the next ".pipe" the way "src("app/images/**/*.*")" one more time. Because we don't need to use every next plug for pictures that have already been converted after the previous one plug
+    src("src/images-src/**/*.*")
+      // if we need to use one more plug for another type of images, have to use before the next ".pipe" the way "src("src/images/**/*.*")" one more time. Because we don't need to use every next plug for pictures that have already been converted after the previous one plug
       // we use plug "newer" before every converter-plug
-      .pipe(newer("app/images"))
+      .pipe(newer("src/images"))
       .pipe(avif())
-      .pipe(dest("app/images"))
+      .pipe(dest("src/images"))
   );
 }
 
 function scripts() {
-  return src("app/js/main.js")
+  return src("src/js/main.js", { sourcemaps: true })
     .pipe(concat("main.min.js"))
     .pipe(uglify())
-    .pipe(dest("app/js"))
+    .pipe(dest("src/js", { sourcemaps: "." }))
     .pipe(browserSync.stream());
 }
 
 function styles() {
-  return src("app/scss/style.scss")
+  return src("src/scss/style.scss", { sourcemaps: true })
     .pipe(autoprefixer())
     .pipe(concat("style.min.css"))
     .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(dest("app/css"))
+    .pipe(dest("src/css", { sourcemaps: "." }))
     .pipe(browserSync.stream());
 }
 
@@ -73,14 +56,13 @@ function styles() {
 function watching() {
   browserSync.init({
     server: {
-      baseDir: "app/",
+      baseDir: "src/",
     },
   });
 
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
-  watch(["app/components/**", "app/pages/**"], makePages);
-  watch(["app/*.html"]).on("change", browserSync.reload);
+  watch(["src/scss/style.scss"], styles);
+  watch(["src/js/main.js"], scripts);
+  watch(["src/index.html"]).on("change", browserSync.reload);
 }
 
 function cleanDist() {
@@ -90,17 +72,20 @@ function cleanDist() {
 function building() {
   return src(
     [
-      "app/css/style.min.css",
-      "app/js/main.min.js",
-      "app/images/**/*.*",
-      "app/fonts/*.woff",
-      "app/fonts/*.woff2",
-      "app/*.html",
+      "src/css/style.min.css",
+      "src/js/main.min.js",
+      "src/images/**/*.*",
+      "src/fonts/*.woff",
+      "src/fonts/*.woff2",
     ],
     {
-      base: "app",
+      base: "src",
     }
   ).pipe(dest("dist"));
+}
+
+function buildingPage() {
+  return src("src/index.html").pipe(dest("."));
 }
 
 exports.images = images;
@@ -108,10 +93,12 @@ exports.fonts = fonts;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
-exports.building = building;
-exports.makePages = makePages;
+exports.buildingPage = buildingPage;
 
-// gulp
-exports.default = parallel(images, fonts, makePages, styles, scripts, watching);
-// gulp build >>> dist
-exports.build = series(cleanDist, building);
+// exports.default = parallel(images, fonts, styles, scripts, watching);
+// exports.build = series(cleanDist, building, buildingPage);
+
+exports.build = parallel(cleanDist, styles, scripts, images, fonts);
+exports.dev = parallel(watching, building, buildingPage);
+// обязательно ли watching в dev?
+// а cleanDist в build?
