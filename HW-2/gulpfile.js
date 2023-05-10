9b3d760437a2df1264e8fc72c79203d2eed612b8
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2023-05-10 11:16:54 +0300

    added packets for img

--- HW-2/gulpfile.js
+++ HW-2/gulpfile.js
@@ -6,6 +6,22 @@ const uglify = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 const clean = require("gulp-clean");
+// * !not for all browsers!
+const avif = require("gulp-avif");
+// * I can't inctall it + gulp-cwebp
+// const webp = require("gulp-webp");
+// * gulp-imagemin works with PNG, JPEG, GIF and SVG
+// const imagemin = require("gulp-imagemin");
+
+function images() {
+  return (
+    src(["app/images/**/*.*", "!app/images/**/*.svg"])
+      // if we need to use one more plug for another type of images, have to use before the next ".pipe" the way "src("app/images/**/*.*")" one more time. Because we don't need to use every next plug for pictures that have already been converted after previous one plug
+      .pipe(avif())
+      // ! dist?
+      .pipe(dest("app/images/dist"))
+  );
+}
 
 function scripts() {
   return src("app/js/main.js")
@@ -24,36 +40,43 @@ function styles() {
     .pipe(browserSync.stream());
 }
 
+// watch method + browser
 function watching() {
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
-  watch(["app/*.html"]).on("change", browserSync.reload);
-}
-
-function browsersync() {
   browserSync.init({
     server: {
       baseDir: "app/",
     },
   });
+  watch(["app/scss/style.scss"], styles);
+  watch(["app/js/main.js"], scripts);
+  watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
 function cleanDist() {
   return src("dist").pipe(clean());
 }
 
+// !!! images + ...
 function building() {
-  return src(["app/css/style.min.css", "app/js/main.min.js", "app/*.html"], {
-    base: "app",
-  }).pipe(dest("dist"));
+  return src(
+    [
+      "app/css/style.min.css",
+      "app/js/main.min.js",
+      "app/*.html",
+      "app/images/dist/**/*.*",
+    ],
+    {
+      base: "app",
+    }
+  ).pipe(dest("dist"));
 }
 
+exports.images = images;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
-exports.browsersync = browsersync;
 
-// gulp build
-exports.build = series(cleanDist, building);
 // gulp
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(images, styles, scripts, watching);
+// gulp build >>> dist
+exports.build = series(cleanDist, building);
